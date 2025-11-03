56db9b82280f370ce3b12825c21bfa7d2d428b5f
UJC100 <meetjohnchima@gmail.com>
2025-11-03 23:03:40 +0800

    Added event card

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -17,7 +17,7 @@ const EventCard = ({title, image, slug, location, date, time}: Props) => {
     <Link href={`/events/${slug}`} id="event-card">
     <Image src={image} alt={title} height={300} width={410} className="poster"/>
 
-    <div className="flex flex- gap-2">
+    <div className="flex flex-row gap-2">
         <Image src={"/icons/pin.svg"} alt="location" width={14} height={14}/>
         <p>{location}</p>
     </div>
