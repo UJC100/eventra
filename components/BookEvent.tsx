3f8ccc52c4efb1169df12cf9eafa1b1dc76fe081
UJC100 <meetjohnchima@gmail.com>
2025-11-03 23:02:20 +0800

    Addded booking

--- components/BookEvent.tsx
+++ components/BookEvent.tsx
@@ -26,7 +26,7 @@ const BookEvent = () => {
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
                         id="email"
-                        placeholder="Enter you email address"
+                        placeholder="Enter you email"
                         />
                     </div>
                     <button type="submit" className="button-submit">Submit</button>
